148829802bdacc44262bc1faed8ba218abad7899
Jillianmwangi <mwangyjill@gmail.com>
2023-02-22 09:04:01 +0300

    commit new files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../src/App.css"
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
