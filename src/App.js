fccbee64f231cb69bbb67cb17482779d81588e02
Jillianmwangi <mwangyjill@gmail.com>
2023-02-23 08:55:20 +0300

    passed data as props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "../src/App.css"
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import info from "./components/Info";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Main/>
+      <Main info1 = {info.data1} info2 = {info.data2} info3 = {info.data3} />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,13 @@
 import "./Main.css";
 import React from "react";
 
-function Main() {
+function Main({info1, info2, info3}) {
   return (
     <div className="container">
       <div className="leftSide">
-        <p className="toptext">SO, YOU WANT TO TRAVEL TO </p>
-        <p className="text2">Space</p>
-        <p className="text3">
-          Let's face it; if you want to go to space, you might as well <br /> genuinely
-          go to outer space and not hover kind of on the <br /> edge of it. Well sit
-          back, and relax because we'll give you a <br /> truly out of this world
-          experience! Explore
-        </p>
+        <p className="toptext">{info1}</p>
+        <p className="text2">{info2}</p>
+        <p className="text3">{info3}</p>
       </div>
 
           <div className="rightSide">
